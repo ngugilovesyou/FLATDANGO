581b3b28542e90a61d8918567443e095ca657054
Samuel <amsamgitau@gmail.com>
2024-07-14 20:25:12 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-
+  fetchFilms()
 })
 
 const list = document.getElementById("list-movies")
@@ -16,8 +16,42 @@ function fetchFilms(){
   fetch('http://localhost:3000/films')
   .then((resp)=>resp.json())
   .then((data)=>{
-    list.innerHTML=''  
-    const li = document.createElement('li')
-    li
-  })
+    list.innerHTML='' 
+    data.forEach(film=>{
+      const li = document.createElement('li');
+      li.textContent = film.title;
+      li.classList.add('film', 'item');
+      li.addEventListener('click', () => getFilmDetails(film));
+      list.appendChild(li);
+  });
+
+  if (data.length > 0) {
+      getFilmDetails(data[0]);
+  }
+});
 }
+
+const getFilmDetails = (film)=>{
+  poster.src = film.poster;
+  title.textContent = film.title;
+  runtime.textContent = `Runtime: ${film.runtime} minutes`;
+  capacity.textContent = `Capacity: ${film.capacity}`
+  showtime.textContent = `Showtime: ${film.showtime}`;
+  const tickets_sold = film.capacity - film.tickets_sold;
+  tickets_sold.textContent =`Tickets sold: ${film.tickets_sold}`
+  description.textContent = film.description;
+  
+   button.disabled = tickets_sold === 0;
+   button.addEventListener('click',buyTicket)
+}
+ function buyTicket(){
+  const available = film.capacity - film.tickets_sold;
+        if (available > 0) {
+            film.tickets_sold++;
+            getFilmDetails(film);
+        }
+    }
+
+     
+    
+ 
